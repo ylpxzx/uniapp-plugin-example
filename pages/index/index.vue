7e9c9a17e459368864479d4356e2ac00aace874e
ylpxzx <794859685@qq.com>
2024-12-20 21:58:03 +0800

    初始化uniapp插件模版

---
+++ pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<ewf-demo></ewf-demo>
+			<view>{{ title }}</view>
+		</view>
+	</view>
+</template>
+<script setup>
+import { ref } from 'vue'
+import { onReady } from '@dcloudio/uni-app'
+const title = ref('Hello')
+onReady(() => {
+  console.log('组件onReady')
+})
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ uni_modules/ewf-demo/components/ewf-demo/ewf-demo.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>{{ state.title }}</view>
+</template>
+<script setup>
+import { reactive } from 'vue'
+import { onLoad, onReady } from '@dcloudio/uni-app'
+const state = reactive({
+	title: "插件示例demo"
+})
+onLoad(() => {
+  console.log('插件onLoad')
+})
+
+onReady(() => {
+	console.log('插件onReady')
+})
+</script>
+<style>
+</style>
