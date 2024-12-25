4e2e3cd3a54517814ba562df55f8a7eb0f650cd7
xiezx@motern.com <xiezx@motern.com>
2024-12-25 10:29:52 +0800

    feat: 范围选择器

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,113 +1,102 @@
 <template>
 	<view>
-		<view class="light" style="background-color: white">
-			<wo-radio v-model:options="state.items" v-model:defaultValue="state.default" @on-change="changeEvent">
-			</wo-radio>
-		</view>
-		<view class="light">
-			<wo-radio v-model:options="state.items" v-model:defaultValue="state.default" v-model:styleObj="state.theme.light" v-slot="slotProps" @on-change="changeEvent">
-				<view style="display: flex;">
-					<view>{{ slotProps.data.name }}</view>
-					<view class="tag">{{ slotProps.data.tag }}</view>
-				</view>
-			</wo-radio>
-		</view>
-		<view class="dark">
-			<wo-radio v-model:options="state.items" v-model:defaultValue="state.default" v-model:styleObj="state.theme.dark" v-slot="slotProps" @on-change="changeEvent">
-				<view style="display: flex;">
-					<view>{{ slotProps.data.name }}</view>
-					<view class="tag">{{ slotProps.data.tag }}</view>
-				</view>
-			</wo-radio>
+		<view class="card">
+			<view class="title">示例-时间段选择(携带副标题)：{{ state.selectedTime1 }}</view>
+			<wo-range-selector @change-time="onChangeTime1" v-model:selected-time="state.defaultValue" v-model:options="state.timeNumberList1"></wo-range-selector>
+			<view class="title">示例-时间段选择：</view>
+			<wo-range-selector v-model:selected-time="state.defaultValue" v-model:options="state.timeNumberList2"></wo-range-selector>
+			<view class="title">示例-日期预约：</view>
+			<wo-range-selector v-model:selected-time="state.defaultValue" v-model:options="state.timeNumberList3"></wo-range-selector>
 		</view>
 	</view>
-	
 </template>
 
 <script setup lang="ts">
 	import { reactive } from 'vue';
-	const state = reactive({
-	  items: [{
-				value: '1',
-				name: '苹果味',
-				tag: '饮料'
-			},
-			{
-				value: '2',
-				name: '香蕉味',
-				tag: '酒水'
-			},
-			{
-				value: '3',
-				name: '火龙果味',
-				tag: '饮料'
-			},
-			{
-				value: '4',
-				name: '西瓜味',
-				tag: '饮料'
-			},
-			{
-				value: '5',
-				name: '哈密瓜味',
-				tag: '酒水'
-			},
-			{
-				value: '6',
-				name: '榴莲味',
-				tag: '酒水'
-		}],
-		default: '2',
-		theme: {
-				light: {
-					primary: 'blue',
-					unselectedRadioBg: '#eaeaea',
-					selectedBg: 'hsla(0,0%,100%,0.5)',
-					height: 20
-				},
-				dark: {
-					primary: 'blue',
-					unselectedRadioBg: 'hsl(223,90%,30%)',
-					selectedBg: 'hsla(223,90%,30%,0.5)',
-					height: 20
-				}
-		},
-		height: 12
-	});
-	const changeEvent = (el) => {
-		console.log('点击：', el);
+	const generateArray = (start: number, end: number) => {
+	  return Array.from(new Array(end + 1).keys()).slice(start);
+	};
+	const genTestOptions1 = () => {
+		const disabledList = [0, 1, 2, 3, 4, 13, 14]
+		const timeOptions = [];
+		const allNumber = generateArray(0, 23);
+		allNumber.forEach((item) => {
+		  if (disabledList.includes(item)) {
+		    timeOptions.push({
+		      disabled: true,
+		      label: `${item}:00`,
+					subtitle: `${item + 1}:00`,
+					value: item
+		    });
+		  } else {
+		    timeOptions.push({
+		      disabled: false,
+		      label: `${item}:00`,
+		      subtitle: `${item + 1}:00`,
+					value: item
+		    });
+		  }
+		});
+		return timeOptions
 	}
+	const genTestOptions2 = () => {
+		const timeOptions = [];
+		const allNumber = generateArray(0, 23);
+		allNumber.forEach((item, index) => {
+		  timeOptions.push({
+		    disabled: false,
+		    label: `${item}:00`,
+		  	value: item
+		  });
+		});
+		return timeOptions
+	}
+	const genTestOptions3 = () => {
+		const disabledList = [4, 5, 6, 13, 14, 15]
+		const timeOptions = [];
+		const allNumber = generateArray(1, 30);
+		allNumber.forEach((item, index) => {
+		  if (disabledList.includes(item)) {
+		    timeOptions.push({
+		      disabled: true,
+		      label: item,
+					subtitle: '约满',
+					value: item
+		    });
+		  } else {
+		    timeOptions.push({
+		      disabled: false,
+		      label: item,
+		      subtitle: '预约',
+					value: item
+		    });
+		  }
+		});
+		return timeOptions
+	}
+	const state = reactive({
+	  defaultValue: [9, 11] as any[],
+		timeNumberList1: genTestOptions1(),
+		timeNumberList2: genTestOptions2(),
+		timeNumberList3: genTestOptions3(),
+	  nowDay: new Date(),
+		selectedTime1: [9, 11] as any[],
+	});
+	const onChangeTime1 = (e: any) => {
+	  if (e.length) {
+	    state.selectedTime1 = e;
+	  }
+	};
 </script>
 
 <style lang="scss" scoped>
-	.light {
-		border-radius: 10px;
-		padding: 20rpx;
-		font-size: 24rpx;
-		background-color: hsl(223,90%,90%);
-		margin: 20px;
-		height: 300px;
-		overflow: auto;
-	}
-	.dark {
-		border-radius: 10px;
-		padding: 20rpx;
-		font-size: 24rpx;
-		background-color: hsl(223,90%,10%);
-		color: white;
-		margin: 20px;
-		height: 300px;
-		overflow: auto;
-	}
-	.tag {
-		background-color: #1BA035;
-		color: white;
-		font-size: 10px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border-radius: 4px;
-		padding: 0 4px;
-		margin-left: 5px;
+	.card {
+		margin: 20rpx;
+		.title {
+			margin-top: 40rpx;
+			font-size: 28rpx;
+			font-weight: 600;
+			padding: 20rpx;
+		}
 	}
 </style>
