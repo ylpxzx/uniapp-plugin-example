ce31392ae8f73efdc2c9eba567aa89b65843fa20
ylpxzx <794859685@qq.com>
2025-06-09 19:26:54 +0800

    feat: add auto border-radius and width props

--- uni_modules/wo-toolbar/pages/index.vue
+++ uni_modules/wo-toolbar/pages/index.vue
@@ -1,15 +1,23 @@
 <template>
-	<view style="padding: 10rpx; font-size: 2em; height: 60vh;" class="flex-center">
-		<view>
-			<view class="flex-center">
-				<view>
-					<ToolbarComp orientation="vertical"></ToolbarComp>
+	<view style="padding: 0.4em; font-size: 1.7em; height: 60vh;">
+		<view class="flex-center">
+				<view class="bg-circle">
+					<ToolbarComp style="border-radius: inherit;" orientation="vertical"></ToolbarComp>
 				</view>
 			</view>
-			<view class="flex-center" style="padding-top: 60px;">
-				<view>
-					<ToolbarComp default-value="facebook"></ToolbarComp>
-				</view>
+		<view class="flex-center" style="padding-top: 60px;">
+			<view class="flex-center bg-circle">
+				<ToolbarComp style="border-radius: inherit;" default-value="browsers"></ToolbarComp>
+			</view>
+		</view>
+		<view class="flex-center" style="padding-top: 60px; width: 100%;">
+			<view style="width: 100%;" class="flex-center bg-circle">
+				<ToolbarComp style="border-radius: inherit;" default-value="iphone" :options="toolbarOptions" @onChange="handleChange"></ToolbarComp>
+			</view>
+		</view>
+		<view class="flex-center" style="padding-top: 60px; width: 100%;">
+			<view style="width: 100%;" class="flex-center bg-react">
+				<ToolbarComp style="border-radius: inherit;" default-value="facebook" :options="toolbarOptions"></ToolbarComp>
 			</view>
 		</view>
 	</view>
@@ -22,11 +30,19 @@ export default {
 		ToolbarComp
 	},
 	data() {
-		return {}
+		return {
+			toolbarOptions: [
+        { icon: "https://img.icons8.com/?size=100&id=56579&format=png&color=ffffff", label: "Unsplash", value: "unsplash" },
+        { icon: "https://img.icons8.com/?size=100&id=890&format=png&color=ffffff", label: "iphone", value: "iphone" },
+        { icon: "https://img.icons8.com/?size=100&id=12412&format=png&color=ffffff", label: "Browsers", value: "browsers" },
+        { icon: "https://img.icons8.com/?size=100&id=118504&format=png&color=ffffff", label: "Facebook", value: "facebook" },
+        { icon: "https://img.icons8.com/?size=100&id=3529&format=png&color=ffffff", label: "Pinterest", value: "pinterest" },
+      ]
+		}
 	},
 	methods: {
-		onChange(value) {
-			console.log('Selected star rating:', value);
+		handleChange(e) {
+			console.log('selected：', e);
 		}
 	}
 }
@@ -37,5 +53,13 @@ export default {
 	display: flex;
 	justify-content: center;
 	align-items: center;
+}
+.bg-circle {
+	background-color: hsl(0, 0%, 10%);
+	border-radius: 1em;
+}
+.bg-react {
+	background-color: hsl(0, 0%, 10%);
+	border-radius: 0.4em;
 }
 </style>
