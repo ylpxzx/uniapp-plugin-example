7078b1f09c00816b3427fba79ed000f080ab9360
ylpxzx <794859685@qq.com>
2025-10-31 17:37:54 +0800

    feat: 选项卡组件实现

---
+++ uni_modules/template-express/pages/index.vue
@@ -0,0 +1,209 @@
+<template>
+  <view class="card">
+    <view>
+      <view class="card-tabs">
+        <view class="tab-content">
+          <view class="tab-content-bg">
+            <view
+              class="tab-normal"
+              v-for="item in tabOptions"
+              :key="item.type"
+              @click="onChange(item.type)"
+            >
+              <view class="tab-title">{{ item.title }}</view>
+            </view>
+            <view class="tab-highlight" :style="tabStyle">
+              <view
+                class="tab-highlight-base flex-center"
+                :class="[
+                  activeTab === 'send'
+                    ? 'tab-highlight-left'
+                    : 'tab-highlight-right',
+                ]"
+              >
+                <view
+                  class="tab-title tab-title-active relative"
+                  :style="{
+                    padding: activeTab === 'send' ? '0 80rpx 0 0 ' : '0',
+                  }"
+                  style="transform: translateX(20rpx)"
+                >
+                  <view class="tab-title-active">{{
+                    activeTab === "send" ? "寄快递" : "查快递"
+                  }}</view>
+                  <view class="tab-shadow"></view>
+                </view>
+              </view>
+            </view>
+          </view>
+        </view>
+        <view
+          class="card-content flex-center"
+          :style="{
+            'border-radius': isAnimation
+              ? '0 0 26rpx 26rpx'
+              : activeTab === 'send'
+              ? '0 26rpx 26rpx 26rpx'
+              : '26rpx 0 26rpx 26rpx',
+          }"
+        >
+          <view
+            v-if="activeTab === 'send'"
+            style="height: 300rpx"
+            class="flex-center"
+          >
+            寄件模块
+          </view>
+          <view
+            v-if="activeTab === 'receive'"
+            style="height: 300rpx"
+            class="flex-center"
+          >
+            查件模块
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeTab: "send", // 当前选中的tab
+      tabStyle: {},
+      isAnimation: false,
+      tabOptions: [
+        {
+          type: "send",
+          title: "寄快递",
+        },
+        {
+          type: "receive",
+          title: "查快递",
+        },
+      ],
+    };
+  },
+  methods: {
+    async onChange(type) {
+      if (this.activeTab === type) {
+        return;
+      }
+      this.activeTab = type;
+      this.isAnimation = true;
+      this.tabStyle.width = "100%";
+      this.tabStyle.background = "#fff";
+      this.tabStyle.borderRadius = "26rpx 26rpx 0 0";
+      setTimeout(() => {
+        if (type === "send") {
+          this.tabStyle = {
+            width: "60%",
+            left: "0",
+          };
+        } else if (type === "receive") {
+          this.tabStyle = {
+            width: "60%",
+            right: "0",
+          };
+        }
+        this.isAnimation = false;
+      }, 400);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 40rpx 20rpx;
+  background-color: #f5f5f5;
+  height: 100%;
+}
+.relative {
+  position: relative;
+}
+
+.bg-white {
+  background-color: #ffffff;
+}
+
+.flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-tabs {
+  position: relative;
+  .tab-content {
+    padding: 2rpx;
+    height: 140rpx;
+
+    &-bg {
+      background: #3370ff;
+      border-radius: 26rpx;
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .tab-normal {
+      padding-bottom: 45rpx;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex: 1;
+    }
+  }
+
+  .tab-title {
+    font-size: 32rpx;
+    font-weight: 600;
+    line-height: 32rpx;
+    letter-spacing: 1rpx;
+    color: #e8e8e8;
+  }
+
+  .tab-title-active {
+    position: relative;
+    z-index: 2;
+    color: #333;
+  }
+  .tab-highlight {
+    position: absolute;
+    top: 0;
+    pointer-events: none;
+    width: 60%;
+    transition: width 0.4s ease;
+  }
+  .tab-highlight-base {
+    position: relative;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    height: 100rpx;
+  }
+  .tab-highlight-left {
+    background-image: url(../static/tab.png);
+  }
+  .tab-highlight-right {
+    background-image: url(../static/tab-right.png);
+  }
+
+  .tab-shadow {
+    position: absolute;
+    bottom: 0;
+    z-index: 1;
+    width: 92rpx;
+    height: 10rpx;
+    background: linear-gradient(90deg, #3370ff 0%, rgba(255, 104, 184, 0) 94%);
+    left: 2rpx;
+    bottom: 1rpx;
+  }
+}
+
+.card-content {
+  transform: translateY(-48rpx);
+  background-color: #fff;
+}
+</style>
